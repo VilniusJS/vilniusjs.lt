9d05b669d5bd21c88ab995d64aadec882703811c
Vytautas Jakutis <vytautas@jakut.is>
2023-01-16 16:58:43 +0200

    src: rename Date and sponsors fields, support different templates per data file

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 const { parse } = require('csv-parse/sync')
-const { writeFileSync, readdirSync, readFileSync, write } = require('fs')
+const { writeFileSync, readdirSync, readFileSync } = require('fs')
 
 const readMeetups = () => 
   readdirSync(`${__dirname}/data`)
   .map((filename) => {
-    const page = filename.substr(0, filename.length - '.csv'.length)
+    const basename = filename.substr(0, filename.length - '.csv'.length)
+    const [ page, meetupTemplateId, htmlTemplateId ] = basename.split('_')
     const file = readFileSync(`${__dirname}/data/${filename}`).toString()
     const json = parse(file, {
       columns: true,
@@ -16,16 +17,18 @@ const readMeetups = () =>
     }, {})
     return {
       page,
+      meetupTemplateId,
+      htmlTemplateId,
       replacements,
     }
   })
 const replace = (template, key, value) => template.split(`___${key}___`).join(value)
 const replaceAll = (template, replacements) => Object.entries(replacements).reduce((template, [key, value]) => replace(template, key, value), template)
 
-const meetupTemplate = readFileSync(`${__dirname}/template.meetup.txt`).toString()
-const htmlTemplate = readFileSync(`${__dirname}/template.html.txt`).toString()
-readMeetups().forEach(({ page, replacements }) => {
+readMeetups().forEach(({ page, meetupTemplateId, htmlTemplateId, replacements }) => {
   console.log(`writing ${page}`)
+  const meetupTemplate = readFileSync(`${__dirname}/${meetupTemplateId}.meetup.txt`).toString()
+  const htmlTemplate = readFileSync(`${__dirname}/${htmlTemplateId}.html.txt`).toString()
   writeFileSync(`${__dirname}/../_posts/${page}.html`, replaceAll(htmlTemplate, replacements))
   writeFileSync(`${__dirname}/../meetup.com/${page}.txt`, replaceAll(meetupTemplate, replacements))
 })
